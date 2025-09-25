b3a32a43f6f616eefe14df91a7a02d3ac573f54b
djibriel <gol.d.jibriel@gmail.com>
2025-09-25 10:58:06 +0700

    tugas 1

---
+++ ratarata.js
@@ -0,0 +1,12 @@
+let x,y,z;
+x=90;
+y=70;
+z=100;
+let ratarata;
+ratarata = (x+y+z)/3;
+console.log(ratarata);
+if (ratarata>=80){
+    console.log("Lulus");
+} else {
+    console.log("Tidak Lulus");
+}
